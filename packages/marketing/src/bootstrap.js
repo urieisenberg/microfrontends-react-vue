bb7b7ad34f143ea559597b14e3b1211c7034ffd2
urieisenberg <urieisenberg@gmail.com>
2023-01-11 17:38:52 +0200

    mount function w react

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 //mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<h1>Hi there!</h1>, el);
+};
 
 //if we are in development and in isolation, call mount immediately
+
 
 //otherwise, run through container and export the mount function
