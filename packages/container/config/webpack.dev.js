82077d9dd32c2cbd0589089c93c1e36659c078f5
urieisenberg <urieisenberg@gmail.com>
2023-01-11 20:54:34 +0200

    clean htmlwebpack, now shared from common config

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -20,9 +19,6 @@ const devConfig = {
       },
       shared: packageJson.dependencies,
       //shared dependencies with container app (host) to avoid duplication of code in the browser (bundle size) and to avoid version conflicts
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
