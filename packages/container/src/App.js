03b68065150caeaee4a1c3fd1da4692e2b521e93
urieisenberg <urieisenberg@gmail.com>
2023-01-11 19:53:51 +0200

    import MarketingApp to use mount

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import {mount} from 'marketing/MarketingApp';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
