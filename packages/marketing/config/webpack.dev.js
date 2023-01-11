9ca7e3bd8b6ea4eb6342e9fe08e73da79d01be28
urieisenberg <urieisenberg@gmail.com>
2023-01-11 20:04:58 +0200

    shared module from package.json

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -18,7 +19,7 @@ const devConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
-      shared: ['react', 'react-dom'], //shared dependencies with container app (host) to avoid duplication of code in the browser (bundle size) and to avoid version conflicts 
+      shared: packageJson.dependencies, //shared dependencies with container app (host) to avoid duplication of code in the browser (bundle size) and to avoid version conflicts
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
