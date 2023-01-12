523b95077c98437f70046e45559dcd43df7294c7
urieisenberg <urieisenberg@gmail.com>
2023-01-12 14:47:40 +0200

    starting auth project - copy files from marketing

---
+++ packages/auth/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
+export default function App({ history }) {
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Router>
+    </StylesProvider>
+  );
+}
