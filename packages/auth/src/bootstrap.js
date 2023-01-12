c43e7418de9645dd3e1c0e58e5ab1684a07fd5cd
urieisenberg <urieisenberg@gmail.com>
2023-01-12 15:50:34 +0200

    adding initial state to memory history

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory(); 
-  
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+
   if (onNavigate) history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
