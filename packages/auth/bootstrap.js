1b409232e620b28c7c628671a05419e3ebd75ef1
urieisenberg <urieisenberg@gmail.com>
2023-01-12 14:52:02 +0200

    clean n match query selector to auth

--- packages/auth/bootstrap.js
+++ packages/auth/bootstrap.js
@@ -3,36 +3,31 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-//mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory }) => {
   const history = defaultHistory || createMemoryHistory(); //if no defautlHistory, then create a memory history object  to use in the app  (instead of browser history)
- 
-  if (onNavigate) history.listen(onNavigate); //if onNavigate is passed in, listen for changes in the history object and call onNavigate  (this is passed in from the container)
- 
+
+  if (onNavigate) history.listen(onNavigate);
+
   ReactDOM.render(<App history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
-      //function to be called by the container when the history object changes
       const { pathname } = history.location;
       if (pathname !== nextPathname) {
-        history.push(nextPathname); //push the next pathname to the history object if it is different from the current pathname
+        history.push(nextPathname);
       }
     },
   };
 };
 
-//if we are in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === 'development') {
- 
-  const devRoot = document.querySelector('#_marketing-dev-root');
- 
+  const devRoot = document.querySelector('#_auth-dev-root');
+
   if (devRoot) {
     mount(devRoot, {
-      defaultHistory: createBrowserHistory(), //use browser history in development  (instead of memory history)
+      defaultHistory: createBrowserHistory(),
     });
   }
 }
 
-//otherwise, run through container and export the mount function
 export { mount };
