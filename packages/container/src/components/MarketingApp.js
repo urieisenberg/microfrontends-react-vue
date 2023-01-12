71437551a415a02e4bab7aa60b93d2705618de1e
urieisenberg <urieisenberg@gmail.com>
2023-01-12 14:41:02 +0200

    container to child communication - listen

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -7,7 +7,7 @@ export default () => {
   const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
         if (pathname !== nextPathname) {
@@ -15,7 +15,9 @@ export default () => {
         }
       },
     });
-  }, []);
+
+    history.listen(onParentNavigate); //listen for changes in the history object and call onParentNavigate  (this is passed in from the marketing app)  
+  });
 
   return <div ref={ref} />;
 };
