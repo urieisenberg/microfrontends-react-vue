6f0bcde3e0860fb39663cc338c086d1ae28e891f
urieisenberg <urieisenberg@gmail.com>
2023-01-12 14:35:05 +0200

    syncing history objects

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,20 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname); //push the next pathname to the history object if it is different from the current pathname
+        }
+      },
+    });
   }, []);
 
   return <div ref={ref} />;
