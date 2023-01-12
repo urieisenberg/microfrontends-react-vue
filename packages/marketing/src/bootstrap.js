494842ffb289b331b1f406094e91c35f05719d8d
urieisenberg <urieisenberg@gmail.com>
2023-01-12 14:41:24 +0200

    onParentNavigate - to communicate w container

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -8,6 +8,12 @@ const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory(); //create a memory history object  to use in the app  (instead of browser history)
   if (onNavigate) history.listen(onNavigate); //if onNavigate is passed in, listen for changes in the history object and call onNavigate  (this is passed in from the container)
   ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate() {
+      console.log('container just navigated');
+    },
+  };
 };
 
 //if we are in development and in isolation, call mount immediately
