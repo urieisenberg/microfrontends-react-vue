ef5d9d631dff5be930f82ea686693fb098d88f64
urieisenberg <urieisenberg@gmail.com>
2023-01-12 15:01:41 +0200

    text changed

--- packages/marketing/src/components/Landing/Copyright.js
+++ packages/marketing/src/components/Landing/Copyright.js
@@ -8,7 +8,7 @@ export function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <MaterialLink component={Link} to="/" color="inherit">
-        Your Website
+        Uri Microfrontends Project{' '}
       </MaterialLink>{' '}
       {new Date().getFullYear()}
       {'.'}
