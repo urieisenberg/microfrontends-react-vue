7ac47e43dc0c6eff191bc86f16eef4301818aa41
urieisenberg <urieisenberg@gmail.com>
2023-01-12 13:15:26 +0200

    import header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import { Header } from './components/Header';
 
 export default () => {
   return (
     <div>
-      <></>
+      <Header />
       <MarketingApp />
     </div>
   );
