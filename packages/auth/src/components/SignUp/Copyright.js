77be2aa6c1b7335562a613b42369ba18a6b8b63c
urieisenberg <urieisenberg@gmail.com>
2023-01-12 15:01:14 +0200

    signup copyright ui

---
+++ packages/auth/src/components/SignUp/Copyright.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+
+export function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link to="/">Uri Microfrontends Project</Link> {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
