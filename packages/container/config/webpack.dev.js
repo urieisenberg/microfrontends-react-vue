09fa47ea6e7e4aa75bf5efef6ef0fdf71fa3e422
urieisenberg <urieisenberg@gmail.com>
2023-01-12 15:07:42 +0200

    / added to publicPath

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -6,7 +6,7 @@ const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8080', 
+    publicPath: 'http://localhost:8080/', 
   },
   devServer: {
     port: 8080,
