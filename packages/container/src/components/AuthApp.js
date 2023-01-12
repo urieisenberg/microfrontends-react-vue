ea265d8a43f47a8a881056349409ddc800126cc8
urieisenberg <urieisenberg@gmail.com>
2023-01-12 15:09:25 +0200

    intergating Auth into Container

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,23 @@
+import { mount } from 'auth/AuthApp';
+import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default () => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname); //push the next pathname to the history object if it is different from the current pathname
+        }
+      },
+    });
+
+    history.listen(onParentNavigate); //listen for changes in the history object and call onParentNavigate  (this is passed in from the Auth app)
+  });
+
+  return <div ref={ref} />;
+};
